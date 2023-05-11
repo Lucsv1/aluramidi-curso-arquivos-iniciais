54abe8425623c4682841a49729d0619fc845695f
Lucas <lcasgalindo@gmail.com>
2023-05-11 09:57:37 -0300

    meiod o projeto

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert('opa');
+function doSound() {
+  const listaSom = document.querySelectorAll("audio");
+  for (let i = 0; i < 9; i++) {
+    return listaSom[i].play();
+  }
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+for (let i = 0; i < 9; i++) {
+  listaDeTeclas[i].onclick = doSound;
+  console.log(i);
+}
