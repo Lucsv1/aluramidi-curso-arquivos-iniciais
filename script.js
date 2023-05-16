31be9e927b51d4799d0e86e137f13ee786bf05d8
Lucas <lcasgalindo@gmail.com>
2023-05-16 10:12:54 -0300

    meio do projeto

--- script.js
+++ script.js
@@ -13,5 +13,10 @@ for (let i = 0; i < listaDeTeclas.length; i++) {
   tecla.onclick = function (){
       doSound(idInstrumento);
   };
-  console.log(i);
+  tecla.onkeydown = () => {
+    tecla.classList.add('ativa');
+  }
+  tecla.onkeyup = () =>{
+    tecla.classList.remove('ativa');
+  }
 }
